b39cb5f9becba073854fab9e775be4d31cc60a7c
ThameezBo <thameez.bodhanya@allangray.co.za>
2021-03-27 13:59:27 +0200

    fix: refactor display answer

--- lib/pages/home/challenge/displayAnswer.dart
+++ lib/pages/home/challenge/displayAnswer.dart
@@ -3,64 +3,60 @@ import 'package:flutter/material.dart';
 import 'package:ninety9names/pages/home/names/details.dart';
 import 'package:ninety9names/repo/name.dart';
 
-displayAnswer(Name n, bool isCorrect, bool isEnglish) async {
-  Column title = Column(
+displayAnswer(Name n, bool isCorrect, BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        content: Container(
+          height: MediaQuery.of(context).size.height * 0.6,
+          child: Column(children: _getDialog(n, isCorrect)),
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("Try another"),
+          )
+        ],
+      );
+    },
+  );
+}
+
+List<Widget> _getDialog(Name n, bool isCorrect) {
+  List<Widget> dialog = [
+    ListTile(title: _getTitleWidget(isCorrect)),
+    ListTile(
+        title: Text(
+      "${n.arabic} translates to ${n.meaning}",
+      textAlign: TextAlign.center,
+    )),
+  ];
+  dialog.addAll(displayName(n));
+  return dialog;
+}
+
+Column _getTitleWidget(bool isCorrect) {
+  Color colour = Colors.red;
+  IconData icon = Icons.thumb_down;
+  String text = "Sorry, your answer is incorrect";
+
+  if (isCorrect) {
+    colour = Colors.green;
+    icon = Icons.thumb_up;
+    text = "Correct!";
+  }
+
+  return Column(
     children: [
-      Icon(Icons.thumb_down, color: Colors.red),
+      Icon(icon, color: colour),
       Text(
-        "Sorry, your answer is incorrect",
-        style: TextStyle(fontSize: 20, color: Colors.red),
+        text,
+        style: TextStyle(fontSize: 20, color: colour),
         textAlign: TextAlign.center,
       ),
     ],
   );
-
-  if (isCorrect == true) {
-    title = Column(
-      children: [
-        Icon(Icons.thumb_up, color: Colors.green),
-        Text(
-          "Correct!",
-          style: TextStyle(fontSize: 20, color: Colors.green),
-          textAlign: TextAlign.center,
-        ),
-      ],
-    );
-    // setState(() {
-    //   score++;
-    // });
-  }
-
-  List<Widget> getDialog(Name n) {
-    List<Widget> dialog = [
-      ListTile(title: title),
-      ListTile(
-          title: Text(
-        "${n.arabic} translates to ${n.meaning}",
-        textAlign: TextAlign.center,
-      )),
-    ];
-    dialog.addAll(displayName(n));
-    return dialog;
-  }
-
-  // await showDialog(
-  //   context: context,
-  //   builder: (context) {
-  //     return AlertDialog(
-  //       content: Container(
-  //         height: MediaQuery.of(context).size.height * 0.9,
-  //         child: Column(children: getDialog(n)),
-  //       ),
-  //       actions: [
-  //         ElevatedButton(
-  //           onPressed: () {
-  //             Navigator.pop(context);
-  //           },
-  //           child: Text("Try another"),
-  //         )
-  //       ],
-  //     );
-  //   },
-  // );
 }
